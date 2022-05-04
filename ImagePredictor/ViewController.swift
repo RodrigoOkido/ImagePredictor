e02a9e602d34d0add229a46ba03eef24f3b8fa33
RodrigoOkido <rodrigookido@gmail.com>
2022-05-04 16:23:25 -0300

    ImagePredictor using MachineLearning

--- ImagePredictor/ViewController.swift
+++ ImagePredictor/ViewController.swift
@@ -6,14 +6,58 @@
 //
 
 import UIKit
+import AVKit
+import Vision
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, AVCaptureVideoDataOutputSampleBufferDelegate {
 
+    
+    @IBOutlet weak var imagePredictorLabel: UILabel!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        let captureSession = AVCaptureSession()
+        captureSession.sessionPreset = .photo
+        
+        guard let captureDevice = AVCaptureDevice.default(for: .video) else {return}
+        guard let input = try? AVCaptureDeviceInput(device: captureDevice) else {return}
+        
+        captureSession.addInput(input)
+        captureSession.startRunning()
+        
+        
+        let previewLayer = AVCaptureVideoPreviewLayer(session: captureSession)
+        view.layer.addSublayer(previewLayer)
+        previewLayer.frame = view.frame
+        
+        let dataOutput = AVCaptureVideoDataOutput()
+        dataOutput.setSampleBufferDelegate(self, queue: DispatchQueue(label: "videoQueue"))
+        captureSession.addOutput(dataOutput)
+    }
+    
+    
+    func captureOutput(_ output: AVCaptureOutput, didOutput sampleBuffer: CMSampleBuffer, from connection: AVCaptureConnection) {
+        guard let pixelBuffer: CVPixelBuffer = CMSampleBufferGetImageBuffer(sampleBuffer) else {return}
+        
+        guard let model = try? VNCoreMLModel(for: SqueezeNet().model) else {return}
+        let request = VNCoreMLRequest(model: model) { (finished, error) in
+            
+            guard let results = finished.results as? [VNClassificationObservation] else {return}
+            
+            guard let firstResult = results.first else {return}
+            DispatchQueue.main.async {
+                self.imagePredictorLabel.text = "\(firstResult.confidence) - \(firstResult.identifier)"
+            }
+            print(firstResult)
+        }
+
+        try? VNImageRequestHandler(cvPixelBuffer: pixelBuffer, options: [:]).perform([request])
     }
 
 
+
 }
+
 
