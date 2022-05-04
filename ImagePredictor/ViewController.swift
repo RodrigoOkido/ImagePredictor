7faa9a96957302fc7a87ad1afed4ff9d10fef755
RodrigoOkido <rodrigookido@gmail.com>
2022-05-04 12:54:36 -0300

    Initial Commit

---
+++ ImagePredictor/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ImagePredictor
+//
+//  Created by Rodrigo Yukio Okido on 04/05/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
